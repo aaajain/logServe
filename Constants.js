9767dda2dc5745a7f47a21d4f22a7a974305cb90
Vishal Singh <VishalS@cis.cisintranet.com>
2018-07-26 15:57:24 +0530

    constant added

---
+++ Constants.js
@@ -0,0 +1,8 @@
+
+//module.exports = 'Active';
+
+var constants = {
+"jbpmLogPath" :"/home/ec2-user/foundations/JBPM/jbpm-installer/wildfly-10.0.0.Final/standalone/log",
+}
+
+module.exports =constants;
--- logserver.js
+++ logserver.js
@@ -1,13 +1,15 @@
 var express    = require('express')
 var serveIndex = require('serve-index')
+var fixed = require('./Constants.js');
 
 var app = express()
 
 // Serve URLs like /ftp/thing as public/ftp/thing
 // The express.static serves the file contents
 // The serveIndex is this module serving the directory
-app.use('/logs', express.static('/home/ec2-user/foundations/DATA_UPLOAD/RJHSFoundations-DataUploadProject/RJHSFoundations_Utility/logs'), serveIndex('/home/ec2-user/foundations/DATA_UPLOAD/RJHSFoundations-DataUploadProject/RJHSFoundations_Utility/logs', {'icons': true}))
+
+app.use('/logs', express.static('fixed.jbpmLogPath'), serveIndex('fixed.jbpmLogPath', {'icons': true}))
 
 // Listen
-app.listen(8011);
-console.log('server listening in 8011');
+app.listen(8012);
+console.log('server listening in 8012');
